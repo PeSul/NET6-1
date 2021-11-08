5083ff0ef4834f0fdb24fee958ccce26604731f8
Petr Suldovský <usrtep@seznam.cz>
2021-11-08 12:15:02 +0100

    metoda Calc

--- HelloWorld/HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/HelloWorld/Program.cs
@@ -7,10 +7,43 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Jak se jmenuješ");
-                        
+
             string name = Console.ReadLine();
-            string pozdrav = "Ahoj " + name;
-            Console.WriteLine(pozdrav);
+
+            string name5p = "name";
+            if (name == "Petr")
+            {
+                name5p = "Petře ";
+            }
+            else if (name == "Karel")
+            {
+                name5p = "Karle ";
+            }
+            else
+            {
+                name5p = name5p + ", pardon Tvé jméno neumín skolňovat";
+            }
+        }
+
+        static void Hello(string name)
+        {
+            int x = 5;
+            int y = Calc(x);
+
+            Console.WriteLine("Vysledek je:" + y);
+            Console.WriteLine("x:" + x);
+        }
+
+        static int Calc(int x)
+        {
+            //vytvorte metodu ktera pricte vstup vynasobeny 2
+            // a pricte 10
+            int result = x * 2 + 10;
+
+            x = 55;
+            Console.WriteLine("x:" + x);
+            return result;
         }
     }
+
 }
