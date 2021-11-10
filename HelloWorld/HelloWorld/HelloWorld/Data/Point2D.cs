86e37a480e2ced6a3fc3572c16f3311e0db316df
PeSul <usrtep@seznam.cz>
2021-11-10 10:05:02 +0100

    Point2D

---
+++ HelloWorld/HelloWorld/HelloWorld/Data/Point2D.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Data
+{
+    internal class Point2D
+    {
+        public Point2D()
+        {
+
+        }
+        public Point2D(int x, int y)
+        {
+          
+        }
+    }
+}
